79b3c4b689bd16023861f7e9d2acca00559073e7
chichi <chichi@DESKTOP-PRSG3OL>
2024-01-04 14:18:27 -0800

    add

--- infra/docker/web/entrypoint.sh
+++ infra/docker/web/entrypoint.sh
@@ -6,7 +6,7 @@ function apply_path {
     echo "Check that we have WEBAPP_URL vars"
     test -n "$WEBAPP_URL"
 
-    find /app/apps/web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_WEBAPP_URL#$WEBAPP_URL#g"
+    find /app/apps/web/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_WEBAPP_URL_PLACEHOLDER#$WEBAPP_URL#g"
 }
 
 apply_path
